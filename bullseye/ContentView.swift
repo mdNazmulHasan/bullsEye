c6c03bddc594760823f9c2e297c6cf064ff4239a
mdNazmulHasan <najmul.hasan10@gmail.com>
2019-12-15 01:25:32 +0600

    view updated

--- bullseye/ContentView.swift
+++ bullseye/ContentView.swift
@@ -9,20 +9,62 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var alertIsVisible:Bool=false
+    //@State var knockKnockAlertIsVisible:Bool=false
+    
     var body: some View {
         VStack {
-            Text("Welcome to my first app")
-                .fontWeight(.bold)
-                .foregroundColor(Color.green)
-            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
-                Text("Hit me!")
+            VStack {
+                HStack {
+                    Text("Put the bulls eye as close as you can to:")
+                    Text("100")
+                }
+                HStack {
+                    Text("1")
+                    Slider(value: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant(10)/*@END_MENU_TOKEN@*/)
+                    Text("100")
+                }
+                Button(action: {
+                    print("Button pressed")
+                    self.alertIsVisible=true
+                }) {
+                    Text("Hit me!")
+                }
+                .alert(isPresented: $alertIsVisible) { () -> Alert in
+                    return Alert.init(title: Text("Hello There"), message: Text("First  pop"), dismissButton: .default(Text("Awesome")))
+                }
+                
+                HStack {
+                    Button(action: {
+                    }) { Text("Start Over")
+                    }
+                    
+                    Text("Score:")
+                    Text("9999")
+                    Text("Round:")
+                    Text("999:")
+                    Button(action: {
+                    }) { Text("Info")
+                    }
+                    
+                    
+                }
+                /*Button(action: {
+                 self.knockKnockAlertIsVisible=true
+                 }) {
+                 Text("Knock, Knock!")
+                 }
+                 .alert(isPresented: $knockKnockAlertIsVisible) { () -> Alert in
+                 return Alert.init(title: Text("Who's there"), message: Text("Not you"), dismissButton: .default(Text("OW")))
+                 }*/
             }
+            
         }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView().previewLayout(.fixed(width: 568, height: 320))
     }
 }
